2763c17e0f33309c9216aa15a535b095fca6960f
Thollarkings <mediaxtreme1@gmail.com>
2024-09-06 19:04:01 +0800

    Edited Nav buttons

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,7 +6,7 @@ const AboutUs = () => {
         <div className="about-us-container">
             <h2>About Us</h2>
             <p>
-                At Tollars, we aim to simplify your business operations with our intuitive invoice generation tool. Our platform allows users to easily create, preview, and customize invoices, helping businesses stay organized. We also provide helpful resources like sales tax determination and discount basics to streamline financial management. With a focus on flexibility, you can personalize invoices with your company logo, customer information, and even apply taxes and discounts. Our commitment to providing user-friendly tools empowers businesses to focus on what matters most — growing and serving their customers. Learn more about us and our mission to support businesses.
+                At our firm, we aim to simplify your business operations with our intuitive invoice generation tool. Our platform allows users to easily create, preview, and customize invoices, helping businesses stay organized. We also provide helpful resources like sales tax determination and discount basics to streamline financial management. With a focus on flexibility, you can personalize invoices with your company logo, customer information, and even apply taxes and discounts. Our commitment to providing user-friendly tools empowers businesses to focus on what matters most — growing and serving their customers. Learn more about us and our mission to support businesses.
             </p>
             <div className="contact-us">
                 <p>Contact Us: <a href="mailto:Mediaxtreme1@gmail.com">Mediaxtreme1@gmail.com</a></p>
